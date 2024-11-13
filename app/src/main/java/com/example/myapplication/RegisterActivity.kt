e242b36f6c97e949fbb9eb16392d3816a6ba6b7f
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-11-13 18:54:12 +0100

    feat[task_4_navigation]: add the click listeners to log in and register text views. Implement their functionality

---
+++ app/src/main/java/com/example/myapplication/RegisterActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textview.MaterialTextView
+
+class RegisterActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_create_account)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val logInTextButton: MaterialTextView = findViewById(R.id.log_in)
+
+        logInTextButton.setOnClickListener {
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
