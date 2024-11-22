c445ca89340ea07a2aa198d97aa1789035ed990a
SPLINTER\darkw <mykola.horbenko22@gmail.com>
2024-11-22 23:38:41 +0100

    feat[TASK-5]: rework tests

--- app/src/test/java/com/example/myapplication/CredentialsManagerTest.kt
+++ app/src/test/java/com/example/myapplication/CredentialsManagerTest.kt
@@ -1,48 +1,34 @@
 package com.example.myapplication
 
-import org.junit.Test
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
+import com.example.myapplication.CredentialsManager.Companion.isEmailValid
+import com.example.myapplication.CredentialsManager.Companion.isPasswordValid
 
 class CredentialsManagerTest {
-    
+
     @Test
-    fun `test empty email`() {
-        val email = ""
-        assertFalse(isEmailValid(email), "Empty email should be invalid")
+    fun givenEmptyEmail_thenReturnFalse() {
+        assertEquals(false, isEmailValid(""))
     }
 
     @Test
-    fun `test wrong email format`() {
-        val email = "example.com"
-        assertFalse(isEmailValid(email), "Email missing '@' should be invalid")
+    fun givenWrongFormatEmail_thenReturnFalse() {
+        assertEquals(false, isEmailValid("example.com"))
     }
 
     @Test
-    fun `test proper email`() {
-        val email = "test@example.com"
-        assertTrue(isEmailValid(email), "Properly formatted email should be valid")
+    fun givenProperEmail_thenReturnTrue() {
+        assertEquals(true, isEmailValid("test@example.com"))
     }
 
-    // Test empty password
     @Test
-    fun `test empty password`() {
-        val password = ""
-        assertFalse(isPasswordValid(password), "Empty password should be invalid")
-    }
-
-    // Test filled password
-    @Test
-    fun `test filled password`() {
-        val password = "securePassword123"
-        assertTrue(isPasswordValid(password), "Non-empty password should be valid")
+    fun givenFilledPassword_thenReturnTrue() {
+        assertEquals(false, isPasswordValid("securePassword123"))
     }
 
     @Test
     fun givenEmptyPassword_thenReturnFalse() {
-        val credentialsManager = CredentialsManager()
-
-        val isEmailValid = credentialsManager.isEmailValid("")
-
-        assertEquals(false, isEmailValid)
+        assertEquals(false, isPasswordValid(""))
     }
 }
